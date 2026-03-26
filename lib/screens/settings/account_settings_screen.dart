6be7a05ce19b6f7bd61f5b3bfe95fa7658251419
Ithaki <user@ithaki.app>
2026-03-26 15:13:21 +0200

    chore: Update dependencies to latest versions in pubspec.lock and pubspec.yaml

--- lib/screens/settings/account_settings_screen.dart
+++ lib/screens/settings/account_settings_screen.dart
@@ -262,8 +262,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
               const SizedBox(height: 12),
               profile.profileVisible
                   ? IthakiOutlineButton(
-                      'Hide Profile from Employers',
-                      icon: IthakiIcon('eye-closed', size: 14),
+                      'Hide Profile from Employers',                    
                       onPressed: () => _showMakeInvisible(context),
                     )
                   : IthakiOutlineButton(
