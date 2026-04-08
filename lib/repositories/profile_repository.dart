d8ba26963fef3d2de2b4a95a612fe93699d909e4
Pablo Armas <pablo.armas@odyssea.com>
2026-04-08 11:19:36 +0300

    add import

--- lib/repositories/profile_repository.dart
+++ lib/repositories/profile_repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/profile_models.dart';
 
